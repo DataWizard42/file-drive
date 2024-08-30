ac28aeafe35deb0faf0047eee062dfb5f9aadc0c
VAIBHAV MANE <vmane7735@gmail.com>
2024-08-30 12:41:18 +0530

    initial

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,10 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { ConvexClientProvider } from "./ConvexClientProvider";
+import ConvexClientProvider from "./ConvexClientProvider";
+import { Header } from "./header";
+import { Toaster } from "@/components/ui/toaster";
+import { Footer } from "./footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,8 +21,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ConvexClientProvider>{children}</ConvexClientProvider>
-        </body>
+        <ConvexClientProvider>
+          <Toaster />
+          <Header />
+          {children}
+          <Footer />
+        </ConvexClientProvider>
+      </body>
     </html>
   );
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
-import { clerkMiddleware } from '@clerk/nextjs/server'
+import { authMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware({
+export default authMiddleware({
     publicRoutes: ["/"],
 });
 
